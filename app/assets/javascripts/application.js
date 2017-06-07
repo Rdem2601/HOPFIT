07e09f21a85896ec1bf3a1c3d3c398e879ae6821
Romain Demoustier <rdemoustier@hotmail.com>
2017-06-07 17:11:12 +0200

    work done

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,3 +4,18 @@
 //= require underscore
 //= require gmaps/google
 //= require_tree .
+
+// $(function() {
+//   $(window).scroll(function() {
+//     //var pageBottom = $(window).scrollTop() + $(window).height();
+//     var bottom_without_btn = $(window).height() - $(".navbar-wagon").height() - $(".gym-banner").height() - $(".offre-mobile").height();
+//     var bottom_with_btn = $(window).height() - $(".navbar-wagon").height() - $(".gym-banner").height() - $(".offre-mobile").height() - $("#mobilecta").height() + 20;
+//     if($(window).scrollTop() > bottom_without_btn) {
+//       $("#mobilecta").removeClass("btn-mobile");
+//       $("#mobilecta").addClass("btn-mobile-fixed");
+//     } else if($(window).scrollTop() < bottom_with_btn) {
+//       $("#mobilecta").removeClass("btn-mobile-fixed");
+//       $("#mobilecta").addClass("btn-mobile");
+//     };
+//   });
+// });
