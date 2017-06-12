18c066da1b0c57d0071a43026098e7ae3b2e1ca9
Julien Crepin <jppcrepin@gmail.com>
2017-06-12 12:22:35 +0200

    fixed the map et redesigne button and smartphone display

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,3 +39,43 @@ gym_3 = Gym.new(
 )
 gym_3.remote_photo_url = "http://res.cloudinary.com/da6umoo9p/image/upload/v1496825435/gym-room-1178293_640_bblml9.jpg"
 gym_3.save
+
+gym_4 = Gym.new(
+  name: "Fitness Ixelles",
+  address: "Chaussée d'Ixelles, 79, Etterbeek",
+  description: "Petite salle de sport pas loin de la porte de Namur.",
+  opening_hours: "Du lundi au dimanche de 10h à 22h",
+  equipments: "Nombreux cours avec prof certifié disponibles.",
+  services: "Wi-fi, piscine, sauna, jardin, bar.",
+  price: 10,
+  user_id: 12
+)
+gym_4.remote_photo_url = "http://res.cloudinary.com/da6umoo9p/image/upload/v1496825435/gym-room-1178293_640_bblml9.jpg"
+gym_4.save
+
+gym_5 = Gym.new(
+  name: "Fitness Ixelles",
+  address: "Chaussée d'Ixelles, 79, Etterbeek",
+  description: "Petite salle de sport pas loin de la porte de Namur.",
+  opening_hours: "Du lundi au dimanche de 10h à 22h",
+  equipments: "Nombreux cours avec prof certifié disponibles.",
+  services: "Wi-fi, piscine, sauna, jardin, bar.",
+  price: 10,
+  user_id: 1
+)
+gym_5.remote_photo_url = "http://res.cloudinary.com/da6umoo9p/image/upload/v1496825435/gym-room-1178293_640_bblml9.jpg"
+gym_5.save
+
+gym_6 = Gym.new(
+  name: "Fitness Ixelles",
+  address: "Chaussée d'Ixelles, 79, Etterbeek",
+  description: "Petite salle de sport pas loin de la porte de Namur.",
+  opening_hours: "Du lundi au dimanche de 10h à 22h",
+  equipments: "Nombreux cours avec prof certifié disponibles.",
+  services: "Wi-fi, piscine, sauna, jardin, bar.",
+  price: 10,
+  user_id: 2
+)
+gym_6.remote_photo_url = "http://res.cloudinary.com/da6umoo9p/image/upload/v1496825435/gym-room-1178293_640_bblml9.jpg"
+gym_6.save
+
