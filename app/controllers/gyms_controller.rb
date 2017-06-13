a2cb919bd1abf322b3de87c1795bbadbd6608c3f
Julien Crepin <jppcrepin@gmail.com>
2017-06-13 17:34:21 +0200

    centered the search barand designed the buttons

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -1,7 +1,7 @@
 class GymsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :find_gym, only: [:show, :update]
-  layout "gyms-app", only: [ :index ]
+  layout "gyms-app", only: [:search, :index ]
 
   def index
     @gyms = Gym.all
