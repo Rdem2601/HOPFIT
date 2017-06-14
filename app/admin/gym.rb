febae5e472a8a5475ca0af3ed8e1d10a174ce6ee
Romain Demoustier <rdemoustier@hotmail.com>
2017-06-14 11:58:53 +0200

    form done

--- app/admin/gym.rb
+++ app/admin/gym.rb
@@ -2,7 +2,22 @@ ActiveAdmin.register Gym do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
- permit_params :name, :address, :lessons, :photo, :descritpion, :opening_hours, :equipments, :services, :user_id
+  form do |f|
+    f.input :user, as: :select, collection: User.all.map { |u| [u.email, u.id] }
+    f.input :name
+    f.input :address
+    f.input :lessons
+    f.input :photo
+    f.input :description
+    f.input :opening_hours
+    f.input :equipments
+    f.input :services
+    f.input :price
+    f.actions
+  end
+
+
+ permit_params :name, :address, :lessons, :photo, :description, :opening_hours, :equipments, :services, :user_id, :price
 #
 # or
 #
